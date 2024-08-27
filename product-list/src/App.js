62c73b0de7c08d21fdf9dbc38a2bd6a2fffec89f
Olatinwo Olalekan <olalekanabdulnafiu@gmail.com>
2024-08-27 10:03:02 +0100

    first commit

---
+++ product-list/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import ProductList from './components/ProductList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Product</h1>
+      <ProductList/>
+
+
+    </div>
+  );
+}
+
+export default App;
